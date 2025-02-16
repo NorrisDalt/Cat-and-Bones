600215e4eef1f78f16d64f30118e663b23cd8103
BenJustice1213 <be287101@ucf.edu>
2025-02-15 23:35:32 -0500

    Added Idol/Skeletons
    Upon collecting the idol, the six skeletons will awaken and start to chase you. If one touches you, you die.
    
    Also tried adding a "GET OUT OF THERE" text on screen but it wasn't showing up.

--- Project 2/Assets/Scripts/PlayerInventory.cs
+++ Project 2/Assets/Scripts/PlayerInventory.cs
@@ -1,10 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using TMPro;
 
 public class PlayerInventory : MonoBehaviour
 {
     private HashSet<string> collectedKeys = new HashSet<string>();
+    private bool hasIdol = false;
+    public TextMeshProUGUI warningText;
 
     public void CollectKey(string keyID)
     {
@@ -15,5 +19,41 @@ public class PlayerInventory : MonoBehaviour
     public bool HasKey(string keyID)
     {
         return collectedKeys.Contains(keyID);
+    }
+
+    public void CollectIdol()
+    {
+        if (hasIdol) return;
+
+        hasIdol = true;
+        Debug.Log("Collected the Idol!");
+        StartCoroutine(AwakenSkeletonsWithDelay());
+    }
+
+    private IEnumerator AwakenSkeletonsWithDelay()
+    {
+        if (warningText != null)
+        {
+            warningText.text = "GET OUT OF THERE!";
+            warningText.enabled = true;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        if (warningText != null)
+        {
+            warningText.enabled = false;
+        }
+
+        SkeletonAI[] skeletons = FindObjectsOfType<SkeletonAI>();
+        foreach (SkeletonAI skeleton in skeletons)
+        {
+            skeleton.Awaken(transform);
+        }
+    }
+
+    public bool HasIdol()
+    {
+        return hasIdol;
     }
 }
