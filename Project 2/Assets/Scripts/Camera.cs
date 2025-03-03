3aa1261f05a4a690d444cfadbabc66e4f4a50a08
BilboBallins101 <drns14@aol.com>
2025-03-02 22:50:27 -0500

    Hides cursor when playing

--- Project 2/Assets/Scripts/Camera.cs
+++ Project 2/Assets/Scripts/Camera.cs
@@ -9,6 +9,7 @@ public class Camera : MonoBehaviour
         get { return sensitivity; }
         set { sensitivity = value; }
     }
+
     [Range(0.1f, 9f)][SerializeField] float sensitivity = 2f;
     [Tooltip("Limits vertical camera rotation. Prevents the flipping that happens when rotation goes above 90.")]
     [Range(0f, 90f)][SerializeField] float yRotationLimit = 88f;
@@ -17,11 +18,20 @@ public class Camera : MonoBehaviour
     const string xAxis = "Mouse X"; //Strings in direct code generate garbage, storing and re-using them creates no garbage
     const string yAxis = "Mouse Y";
 
+    void Start()
+    {
+        // Hide the cursor when the game starts
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     void Update()
     {
         rotation.x += Input.GetAxis(xAxis) * sensitivity;
         rotation.y += Input.GetAxis(yAxis) * sensitivity;
+
         rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
+
         var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
         var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
 
