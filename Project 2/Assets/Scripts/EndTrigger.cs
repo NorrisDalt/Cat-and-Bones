5cefb1f53ab465c66b81206eab451b4069fded60
BenJustice1213 <be287101@ucf.edu>
2025-03-05 22:53:43 -0500

    Fixed Win Screen From Replaying in Beginning

--- Project 2/Assets/Scripts/EndTrigger.cs
+++ Project 2/Assets/Scripts/EndTrigger.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class EndTrigger : MonoBehaviour
 {
+    private void Start()
+    {
+        Idol.idolCollected = false;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && Idol.idolCollected) // Check if idol is collected
